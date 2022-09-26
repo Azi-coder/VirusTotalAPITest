d642233b9adc2973cc2c05738cb3c9ada7cdc936
Azi-coder <azmarslnts@gmail.com>
2022-09-27 02:50:41 +0300

    message

---
+++ contact_us_form_bdd/src/test/java/stepDefinitions/Steps.java
@@ -0,0 +1,21 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Steps {
+    @Given("Open the website")
+    public void open_the_website() {
+        System.setProperty()
+
+    }
+    @When("Click the contact button")
+    public void click_the_contact_button() {
+
+    }
+    @Then("Go to the contact page")
+    public void go_to_the_contact_page() {
+
+    }
+}
---
+++ contact_us_form_bdd/src/test/resources/Contact.feature
@@ -0,0 +1,6 @@
+Feature: Contact the website
+
+  Scenario: Click the contact button
+    Given Open the website
+    When Click the contact button
+    Then Go to the contact page
