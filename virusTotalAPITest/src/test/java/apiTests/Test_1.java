6b6ff12c00bb4fc6c1b720537082268c9e29a952
Azi-coder <azmarslnts@gmail.com>
2022-10-31 23:13:37 +0300

    virusTotalApiTest

---
+++ virusTotalAPITest/src/test/java/apiTests/Test_1.java
@@ -0,0 +1,19 @@
+package apiTests;
+
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
+
+public class Test_1 {
+    String apiKey = "e7f19ca21d76898500686385a2d934681460c77730edb17a59ce705b9cf3a7d4";
+    String ip = "192.168.1.20";
+
+    @Test
+    public void test_Step_1() {
+        baseURI = "https://www.virustotal.com/api";
+        given().header("x_apikey", apiKey).when().get("/v3/ip_addresses/" + ip).then().statusCode(200)
+                .body("data.attributes.last_analysis_stats.suspicious",equalTo(0))
+                .log().all();
+    }
+}
